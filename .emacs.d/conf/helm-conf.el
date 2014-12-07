3071df21d50fd02ad928cd1ea2a895c28b54ae84
Sorin Dumitru <sorin@returnze.ro>
2014-12-07 10:59:03 +0200

    emacs: only disable gui elements when using a gui

--- .emacs.d/conf/helm-conf.el
+++ .emacs.d/conf/helm-conf.el
@@ -5,7 +5,10 @@
 (setq helm-idle-delay 0.1)
 (setq helm-input-idle-delay 0.1)
 
-(setq helm-locate-command "locate %.0s %s")
+(if (string-equal system-type "darwin")
+  (setq helm-locate-command "locate-with-mdfind %.0s %s")
+  (setq helm-locate-command "locate %.0s %s")
+  )
 (dolist (ext '("\\.swf$" "\\.elc$" "\\.pyc$"))
       (add-to-list 'helm-boring-file-regexp-list ext))
 (global-set-key (kbd "C-c C-f") 'helm-for-files)

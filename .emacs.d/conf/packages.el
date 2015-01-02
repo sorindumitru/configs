10e864474cd1993247343836af5f1a8be4f00c89
Sorin Dumitru <sorin@returnze.ro>
2015-01-02 18:19:42 +0200

    remove marmalade repository

--- .emacs.d/conf/packages.el
+++ .emacs.d/conf/packages.el
@@ -2,7 +2,6 @@
 (package-initialize)
 
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (defvar elpa-package-list '(
